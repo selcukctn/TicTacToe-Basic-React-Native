4524fbf4ad13cc5071ce704e914db97aa7444443
Harun Selçuk Çetin <harunselcukctn@gmail.com>
2022-04-10 13:07:06 +0300

    general desing
    genereal desing

---
+++ TicTacToe/src/Routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+
+
+const Routes = () => {
+    return(
+        <View>
+            <Text>Routes</Text>
+        </View>
+    )
+}
+
+
+
+
+export default Routes;
